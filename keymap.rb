6563325e82ca2943901124a1712af3f6657047bf
Mitsutaka Mimura <takkanm@gmail.com>
2022-03-15 22:09:49 +0900

    fix keymap

--- keymap.rb
+++ keymap.rb
@@ -23,22 +23,22 @@ kbd.init_pins(
 
 # default layer should be added at first
 kbd.add_layer :default, %i[
-  KC_ESCAPE KC_Q    KC_W    KC_E        KC_R    KC_T      KC_Y      KC_U      KC_I      KC_O     KC_P      KC_MINUS
-  KC_TAB    KC_A    KC_S    KC_D        KC_F    KC_G      KC_H      KC_J      KC_K      KC_L     KC_SCOLON KC_BSPACE
-  KC_LSFT   KC_Z    KC_X    KC_C        KC_V    KC_B      KC_N      KC_M      KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
-  KC_NO     KC_NO   KC_NO   ALT_AT      KC_LCTL LOWER_SPC RAISE_ENT SPC_CTL   RUBY_GUI  KC_NO    KC_NO     KC_NO
+  KC_TAB   KC_Q  KC_W  KC_E  KC_R      KC_T                      KC_Y    KC_U       KC_I      KC_O     KC_P      KC_BSPACE
+  CTRL_ESC KC_A  KC_S  KC_D  KC_F      KC_G                      KC_H    KC_J       KC_K      KC_L     KC_SCOLON KC_QUOTE
+  KC_LSFT  KC_Z  KC_X  KC_C  KC_V      KC_B                      KC_N    KC_M       KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
+           KC_NO KC_NO KC_NO GUI_LANG2 LOWER KC_SPACE   KC_ENTER RAISE   ALT_LANG1  KC_NO     KC_NO    KC_NO
 ]
 kbd.add_layer :raise, %i[
-  KC_GRAVE  KC_EXLM KC_AT   KC_HASH     KC_DLR  KC_PERC   KC_CIRC   KC_AMPR   KC_ASTER  KC_LPRN  KC_RPRN   KC_EQUAL
-  KC_TAB    KC_LABK KC_LCBR KC_LBRACKET KC_LPRN KC_QUOTE  KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE
-  KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN KC_DQUO   KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
-  KC_NO     KC_NO   KC_NO   ALT_AT      KC_LCTL ADJUST    RAISE_ENT SPC_CTL   KC_RGUI   KC_NO    KC_NO     KC_NO
+  KC_TAB   KC_EXLM  KC_AT  KC_HASH  KC_DLR    KC_PERC                     KC_CIRC  KC_AMPR    KC_ASTER    KC_LPRN     KC_RPRN   KC_BSPACE
+  CTRL_ESC KC_NO    KC_NO  KC_NO    KC_NO     KC_NO                       KC_MINUS KC_EQUAL   KC_LPRN     KC_RPRN     KC_PIPE   KC_GRAVE
+  KC_LSFT  KC_NO    KC_NO  KC_NO    KC_NO     KC_NO                       KC_UNDS  KC_PLUS    KC_LBRACKET KC_RBRACKET KC_BSLASH KC_TILD
+           KC_NO    KC_NO  KC_NO    GUI_LANG2 ADJUST  KC_SPACE   KC_ENTER RAISE    ALT_LANG1  KC_NO       KC_NO       KC_NO
 ]
 kbd.add_layer :lower, %i[
-  KC_ESCAPE KC_1    KC_2    KC_3        KC_4    KC_5      KC_6      KC_7      KC_8      KC_9     KC_0      KC_EQUAL
-  KC_TAB    KC_NO   KC_NO   KC_NO       KC_LPRN KC_QUOTE  KC_DOT    KC_4      KC_5      KC_6     KC_PLUS   KC_BSPACE
-  KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN KC_DQUO   KC_0      KC_1      KC_2      KC_3     KC_SLASH  KC_COMMA
-  KC_NO     KC_NO   KC_NO   ALT_AT      KC_LCTL LOWER_SPC ADJUST    SPC_CTL   KC_RGUI   KC_NO    KC_NO     KC_NO
+  KC_TAB   KC_1  KC_2  KC_3  KC_4      KC_5                      KC_6    KC_7       KC_8      KC_9     KC_0  KC_BSPACE
+  CTRL_ESC KC_NO KC_NO KC_NO KC_NO     KC_NO                     KC_LEFT KC_DOWN    KC_UP     KC_RIGHT KC_NO KC_NO
+  KC_LSFT  KC_NO KC_NO KC_NO KC_NO     KC_NO                     KC_NO   KC_NO      KC_NO     KC_NO    KC_NO KC_NO
+           KC_NO KC_NO KC_NO GUI_LANG2 LOWER KC_SPACE   KC_ENTER ADJUST  ALT_LANG1  KC_NO     KC_NO    KC_NO
 ]
 kbd.add_layer :adjust, %i[
   KC_F1     KC_F2   KC_F3   KC_F4       KC_F5   KC_F6     KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
@@ -47,17 +47,16 @@ kbd.add_layer :adjust, %i[
   KC_NO     KC_NO   KC_NO   ALT_AT      KC_LCTL ADJUST    ADJUST    SPC_CTL   KC_RGUI   KC_NO    KC_NO     KC_NO
 ]
 
-kbd.define_composite_key :SPC_CTL, %i(KC_SPACE KC_RCTL)
-
 #
 #                   Your custom     Keycode or             Keycode (only modifiers)    Release time     Re-push time
 #                   key name        Array of Keycode       or Layer Symbol to be held  threshold(ms)    threshold(ms)
 #                                   or Proc                or Proc which will run      to consider as   to consider as
 #                                   when you click         while you keep press        `click the key`  `hold the key`
-kbd.define_mode_key :ALT_AT,      [ :KC_AT,                :KC_LALT,                   150,             150 ]
-kbd.define_mode_key :RAISE_ENT,   [ :KC_ENTER,             :raise,                     150,             150 ]
-kbd.define_mode_key :LOWER_SPC,   [ :KC_SPACE,             :lower,                     150,             150 ]
-kbd.define_mode_key :RUBY_GUI,    [ Proc.new { kbd.ruby }, :KC_RGUI,                   300,             nil ]
+kbd.define_mode_key :CTRL_ESC,    [ :KC_ESC,               :KC_LCTL,                   150,             150 ]
+kbd.define_mode_key :GUI_LANG2,   [ :KC_LANG2,             :KC_LGUI,                   150,             150 ]
+kbd.define_mode_key :ALT_LANG1,   [ :KC_LANG1,             :KC_RALT,                   150,             150 ]
+kbd.define_mode_key :RAISE,       [ nil,                   :raise,                     nil,             nil ]
+kbd.define_mode_key :LOWER,       [ nil,                   :lower,                     nil,             nil ]
 kbd.define_mode_key :ADJUST,      [ nil,                   :adjust,                    nil,             nil ]
 
 # `before_report` will work just right before reporting what keys are pushed to USB host.
