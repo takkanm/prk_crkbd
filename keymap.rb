9fb11263ebad1d053e316e8fc624cde620753707
Mitsutaka Mimura <takkanm@gmail.com>
2022-07-26 23:05:31 +0900

    add split keyboard setting

--- keymap.rb
+++ keymap.rb
@@ -4,6 +4,9 @@ kbd = Keyboard.new
 kbd.via = true
 
 kbd.via_layer_count = 4
+kbd.split = true
+kbd.mutal_uart_at_my_own_risk = true
+kbd.set_anchor(:right)
 
 kbd.init_pins(
   [ 4, 5, 6, 7 ],            # row0, row1,... respectively
